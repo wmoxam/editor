6ce6ea6c80ba1feb2851310086a61055b272d768
“Wesley <wes@crowdmark.com>
2022-07-23 00:15:51 -0400

    Draw a blank screen, etc

--- src/editor.cr
+++ src/editor.cr
@@ -2,27 +2,74 @@
 # Based on kilo, https://viewsourcecode.org/snaptoken/kilo/index.html
 
 require "termios"
+require "./ioctl"
 
-module Editor
-  VERSION = "0.1.0"
+class Editor
+  property rows : Int32 = 0
+  property columns : Int32 = 0
 
-  def self.run
-    STDIN.raw do |input|
+  def initialize
+    get_window_size!
+  end
+
+  def run
+    STDIN.raw do |io|
       while true
-        c = '0'
-        c = input.read_char
-        if c.nil? || c == 'q'
-          break
-        else
-          if c.ascii_control?
-            print "#{c.bytes.to_s}\r\n"
-          else
-            print "#{c.bytes.to_s} ('#{c}')\r\n"
-          end
+        refresh_screen
+
+        unless process_keypresses(io)
+          refresh_screen
+          Process.exit(0)
         end
       end
     end
   end
+
+  private def ctrl_key(char)
+    char.bytes.first & 0x1f
+  end
+
+  private def drawRows
+    @rows.times do |i|
+      print "~"
+
+      print "\r\n" unless i == @rows - 1
+    end
+  end
+
+  private def process_keypresses(io)
+    c = io.read_char
+
+    unless c.nil?
+      if c.ascii_control?
+        case c.bytes.first
+        when ctrl_key('q')
+          return false
+        else
+          print "#{c.bytes.to_s}\r\n"
+        end
+      else
+        print "#{c.bytes.to_s} ('#{c}')\r\n"
+      end
+    end
+
+    true
+  end
+
+  private def refresh_screen
+    print "\x1b[2J"
+    print "\x1b[H"
+
+    drawRows
+
+    print "\x1b[H"
+  end
+
+  private def get_window_size!
+    LibC.ioctl(1, LibC::TIOCGWINSZ, out screen_size)
+    @rows = screen_size.ws_col.to_i - 1
+    @columns = screen_size.ws_row.to_i - 2
+  end
 end
 
-Editor.run
+Editor.new.run
