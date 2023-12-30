3485a20d8b6aad8b51a8ef6938bbca5a44fbfb17
“Wesley <wes@crowdmark.com>
2023-12-30 01:40:18 -0500

    Cursor movement

--- src/editor.cr
+++ src/editor.cr
@@ -6,6 +6,10 @@ class Editor
 
   property rows : Int32 = 0
   property columns : Int32 = 0
+
+  property cursor_x : Int32 = 1
+  property cursor_y : Int32 = 1
+
   property screen_buffer : String::Builder
 
   def initialize
@@ -32,40 +36,15 @@ class Editor
   private def draw_rows
     rows.times do |i|
       if i == (rows / 3).to_i
-        @screen_buffer << "Wes's editor -- version #{VERSION}"
+        welcome_msg = "Wes's editor -- version #{VERSION}"[0..columns - 1]
+        padding = (columns - welcome_msg.size) / 2
+        @screen_buffer << "#{" " * padding.to_i}#{welcome_msg}"
       else
         @screen_buffer << "~"
       end
       @screen_buffer << "\x1b[K" # erases the part of the line to the right of the cursor
       @screen_buffer << "\r\n" unless i == rows - 1
     end
-  end
-
-  private def process_keypresses(io)
-    c = io.read_char
-
-    unless c.nil?
-      if c.ascii_control?
-        case c.bytes.first
-        when ctrl_key('q')
-          return false
-        end
-      end
-    end
-
-    true
-  end
-
-  private def refresh_screen
-    @screen_buffer = String::Builder.new
-    @screen_buffer << "\x1b[?25l" # hide cursor
-    @screen_buffer << "\x1b[H"    # reposition cursor
-
-    draw_rows
-
-    @screen_buffer << "\x1b[H"    # reposition cursor
-    @screen_buffer << "\x1b[?25h" # show cursor
-    print screen_buffer.to_s
   end
 
   # https://viewsourcecode.org/snaptoken/kilo/03.rawInputAndOutput.html#window-size-the-hard-way
@@ -97,6 +76,65 @@ class Editor
         Process.exit(0)
       end
     end
+  end
+
+  private def editor_move_cursor(key)
+    case key
+    when 'a'
+      @cursor_x -= 1 if @cursor_x > 1
+    when 'd'
+      @cursor_x += 1 if @cursor_x < columns
+    when 'w'
+      @cursor_y -= 1 if @cursor_y > 1
+    when 's'
+      @cursor_y += 1 if @cursor_y < rows
+    end
+  end
+
+  private def process_keypresses(io)
+    c = io.read_char
+
+    unless c.nil?
+      if c.ascii_control?
+        case c.bytes.first
+        when ctrl_key('q')
+          return false
+        when '\e'
+          case io.read_char
+          when '['
+            case io.read_char
+            when 'A'
+              editor_move_cursor 'w'
+            when 'B'
+              editor_move_cursor 's'
+            when 'C'
+              editor_move_cursor 'd'
+            when 'D'
+              editor_move_cursor 'a'
+            end
+          end
+        end
+      else
+        case c.bytes.first
+        when 'w', 's', 'a', 'd'
+          editor_move_cursor c
+        end
+      end
+    end
+
+    true
+  end
+
+  private def refresh_screen
+    @screen_buffer = String::Builder.new
+    @screen_buffer << "\x1b[?25l" # hide cursor
+    @screen_buffer << "\x1b[H"    # reposition cursor
+
+    draw_rows
+
+    @screen_buffer << "\x1b[#{cursor_y};#{cursor_x}H" # reposition cursor
+    @screen_buffer << "\x1b[?25h"                     # show cursor
+    print screen_buffer.to_s
   end
 end
 
